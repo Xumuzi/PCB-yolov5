62ac13082c167c323f9caf2754f4d35c95295d6b
Xumuzi <15210113523@163.com>
2024-05-11 21:43:46 +0800

    first commit

---
+++ 测试torchhub.py
@@ -0,0 +1,15 @@
+# ValueError: check_hostname requires server_hostname 请降低requests版本，这里requests-2.21.0
+import torch
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5x, custom
+
+# Images
+img = 'https://ultralytics.com/images/zidane.jpg'  # or file, PIL, OpenCV, numpy, multiple
+
+# Inference
+results = model(img)
+
+# Results
+results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
+# results.show()
